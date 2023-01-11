c759e13a66759be4ed133c6a24a83db862d39d03
venkat-0706 <119731968+venkat-0706@users.noreply.github.com>
2023-01-11 21:50:58 +0530

    #include<stdio.h> int large(int a) {     int b,i,s,r,lar;     for(b=a+1;b>=0;b++)     {         i=b;           s=0;           while(i!=0)           {               r=i%10;               s=s*10+r;               i=i/10;           }           if(s==b)           {               lar=s;               break;           }     }     return lar; } int small(int a) {     int b,i,r,s,sma;     for(b=a-1;b>0;b--)     {         i=b;           s=0;           while(i!=0)           {               r=i%10;               i=i/10;               s=s*10+r;           }           if(s==b)           {               sma=s;               break;           }     }     return sma; } int main() {     int a,b,c,d,e,f,min;     scanf("%d",&a);     c=large(a);     b=small(a);     e=c-a;     f=a-b;     if(e==f)     {         printf("%d %d",b,c);     }     else if(e<f)     {         printf("%d",c);     }     else     {         printf("%d",b);     } }

---
+++ Closest_Palindrome_.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int large(int a)
+{
+    int b,i,s,r,lar;
+    for(b=a+1;b>=0;b++)
+    {
+        i=b;
+          s=0;
+          while(i!=0)
+          {
+              r=i%10;
+              s=s*10+r;
+              i=i/10;
+          }
+          if(s==b)
+          {
+              lar=s;
+              break;
+          }
+    }
+    return lar;
+}
+int small(int a)
+{
+    int b,i,r,s,sma;
+    for(b=a-1;b>0;b--)
+    {
+        i=b;
+          s=0;
+          while(i!=0)
+          {
+              r=i%10;
+              i=i/10;
+              s=s*10+r;
+          }
+          if(s==b)
+          {
+              sma=s;
+              break;
+          }
+    }
+    return sma;
+}  
+int main()
+{
+    int a,b,c,d,e,f,min;
+    scanf("%d",&a);
+    c=large(a);
+    b=small(a);
+    e=c-a;
+    f=a-b;
+    if(e==f)
+    {
+        printf("%d %d",b,c);
+    }
+    else if(e<f)
+    {
+        printf("%d",c);
+    }
+    else
+    {
+        printf("%d",b);
+    }
+}
