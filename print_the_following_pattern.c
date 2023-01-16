6fb7e09e1efbe09934b30ce361ceed0aa4217229
venkat-0706 <119731968+venkat-0706@users.noreply.github.com>
2023-01-16 10:45:11 +0530

    #include<stdio.h> int main() {     int i,j,n,t;     scanf("%d",&n);     for(i=1,t=(64+i);i<=n;i++,t++)     {         for(j=1;j<=n;j++)         {             printf("%c ",t);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,k,n;
+    int i,j,n,t;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=1,t=(64+i);i<=n;i++,t++)
     {
-        for(j=1;j<=n-i;j++)
+        for(j=1;j<=n;j++)
         {
-            printf(" ");
-         }
-    for(k=1;k<2*i;k++)
-    {
-        printf("%d",i);
-    }
-    printf("
+            printf("%c ",t);
+        }
+        printf("
 ");
-}
+    }
 }
