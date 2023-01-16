c375abd85863577c6de3f1bf6f5b0eb36fabf792
venkat-0706 <119731968+venkat-0706@users.noreply.github.com>
2023-01-16 10:34:50 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             if((j==1)||(i==n)||(i==j))             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,15 +5,15 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n;j++)
+        for(j=1;j<=i;j++)
         {
-            if(i==j)
+            if((j==1)||(i==n)||(i==j))
             {
-                printf("0");
+                printf("*");
             }
             else
             {
-                printf("x");
+                printf(" ");
             }
         }
         printf("
