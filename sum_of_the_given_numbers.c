0efb48f665b00071caa35e168f05dbdd6389a130
venkat-0706 <119731968+venkat-0706@users.noreply.github.com>
2023-05-26 14:30:49 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     for(int i=1;i<=t;i++)     {         int a,b,sum;         scanf("%d %d",&a,&b);         sum=a+b;         printf("%d ",sum);     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for(int i=1;i<=t;i++)
+    {
+        int a,b,sum;
+        scanf("%d %d",&a,&b);
+        sum=a+b;
+        printf("%d
+",sum);
+    }
+}
