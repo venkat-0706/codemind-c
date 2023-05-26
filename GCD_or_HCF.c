cbaebf88e48f48ac38ee30fe8e757329e1cb90ce
venkat-0706 <119731968+venkat-0706@users.noreply.github.com>
2023-05-26 15:30:56 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d %d",&a,&b);     while(a!=b)     {         if(a>b)         {             a-=b;         }         else         {             b-=a;         }     }     printf("%d",a); }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    while(a!=b)
+    {
+        if(a>b)
+        {
+            a-=b;
+        }
+        else
+        {
+            b-=a;
+        }
+    }
+    printf("%d",a);
+}
